71bf2b1b5d02fc519f0e8b3d69e1bc2f6a1a5ad7
Julie Nisbet <jmnisbet@gmail.com>
2022-12-02 15:23:21 -0800

    adding start to the secrets route

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,8 +2,23 @@
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS users CASCADE;
 
+DROP TABLE IF EXISTS secrets;
+
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   email VARCHAR UNIQUE,
   password_hash VARCHAR NOT NULL
 );
+
+CREATE TABLE secrets (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  title VARCHAR,
+  description VARCHAR,
+  created_at TIMESTAMP DEFAULT NOW()
+);
+
+INSERT INTO
+  secrets (title, description)
+VALUES
+  ('Shhhhhh', 'Top Secret: Benny is adorable'),
+  ('AREA 52', 'Aliens are real.');
